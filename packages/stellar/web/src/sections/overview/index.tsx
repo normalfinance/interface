28ebfd041c789013fe3a275c8f08b2d0f2ab8931
Niko Gorjan <niko.gorjan@gmail.com>
2025-02-06 07:42:06 +0100

    feat: Implement TradingVolume component

--- packages/stellar/web/src/sections/overview/index.tsx
+++ packages/stellar/web/src/sections/overview/index.tsx
@@ -4,8 +4,13 @@ import Grid2 from '@mui/material/Grid2';
 import { AssetsAndLiabilities } from '@/components/_overview-page-components/assets-and-liabilities/assets-and-liabilities';
 import { PortfolioValue } from '@/components/_overview-page-components/portfolio-value/portfolio-value';
 import { DashboardContent } from '@/layouts/dashboard';
+import { TradingVolume } from '@/components/_overview-page-components/trading-volume/trading-volume';
+import { useTheme } from '@mui/material/styles';
+import { Stack, Typography } from '@mui/material';
 
 export default function OverviewView() {
+  const theme = useTheme();
+
   const _appRelated = [
     {
       id: '1',
@@ -25,8 +30,15 @@ export default function OverviewView() {
 
   return (
     <DashboardContent maxWidth="xl">
-      <h1>Testing</h1>
-      <Grid2 container spacing={3}>
+      <Stack spacing={1}>
+        <Typography variant="h4" color="text.primary">
+          Welcome back 👋
+        </Typography>
+        <Typography variant="body1" color="text.secondary">
+          Account Overview
+        </Typography>
+      </Stack>
+      <Grid2 container spacing={3} sx={{ mt: 3 }}>
         <Grid2 size={{ xs: 12, md: 8 }}>
           <PortfolioValue
             id="demo__4"
@@ -101,6 +113,19 @@ export default function OverviewView() {
 
         <Grid2 size={{ xs: 12, md: 4 }}>
           <AssetsAndLiabilities title="Assets & Liabilities" list={_appRelated} />
+        </Grid2>
+      </Grid2>
+
+      {/* Second row: TradingVolume items */}
+      <Grid2 container spacing={3} sx={{ mt: 3 }}>
+        <Grid2 size={{ xs: 12, md: 4 }}>
+          <TradingVolume title="Total 24h Trading Volume" percent={0.6} total={212350000} />
+        </Grid2>
+        <Grid2 size={{ xs: 12, md: 4 }}>
+          <TradingVolume title="Total 30d Trading Volume" percent={-80.6} total={5690000000} />
+        </Grid2>
+        <Grid2 size={{ xs: 12, md: 4 }}>
+          <TradingVolume title="Total Value Locked" percent={0.6} total={883470000} />
         </Grid2>
       </Grid2>
     </DashboardContent>
