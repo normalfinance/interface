3aff24c5ac226f15ef72675f3e8a9c8c997a7e59
Niko Gorjan <niko.gorjan@gmail.com>
2025-02-15 07:30:36 +0100

    feat(TradingVolume): trading volume component is no longer needed since we now use reuseable component stat-card

--- packages/stellar/web/src/sections/overview/index.tsx
+++ packages/stellar/web/src/sections/overview/index.tsx
@@ -4,7 +4,6 @@ import Grid2 from '@mui/material/Grid2';
 import { AssetsAndLiabilities } from '@/components/_overview-page-components/assets-and-liabilities/assets-and-liabilities';
 import { PortfolioValue } from '@/components/_overview-page-components/portfolio-value/portfolio-value';
 import { DashboardContent } from '@/layouts/dashboard';
-import { TradingVolume } from '@/components/_overview-page-components/trading-volume/trading-volume';
 import { Stack, Typography, useTheme } from '@mui/material';
 import { Markets } from '@/components/_overview-page-components/markets/markets';
 import { MarketTable } from '@/components/_overview-page-components/market-table/market-table';
