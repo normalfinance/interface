4963f2f93c98063cd00d714171a2c199e220fe52
Joshua Blew <joshua@normalfinance.io>
2025-02-24 15:06:03 -0600

    feat: add missing /routes folder

--- packages/solana/src/routes/paths.ts
+++ packages/solana/src/routes/paths.ts
@@ -1,17 +1,17 @@
+import { kebabCase } from 'es-toolkit';
 
 // ----------------------------------------------------------------------
 
 export const paths = {
-  comingSoon: '/coming-soon',
-  maintenance: '/maintenance',
-  pricing: '/pricing',
-  payment: '/payment',
-  about: '/about-us',
-  contact: '/contact-us',
-  faqs: '/faqs',
-  page403: '/error/403',
   page404: '/error/404',
   page500: '/error/500',
-  components: '/components',
-  docs: 'https://docs.minimals.cc',
+  docs: 'https://docs.normalfinance.io',
+  overview: '/overview',
+  markets: {
+    root: '/markets',
+    details: (title: string) => `/markets/${kebabCase(title)}`,
+  },
+  insurance: '/insurance',
+  rewards: '/rewards',
+  analytics: '/analytics',
 };
