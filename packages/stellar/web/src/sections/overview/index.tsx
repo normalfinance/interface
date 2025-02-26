87e5483aa089f80e91bce48ad177fca589bb3c99
nikogorjan <63477501+nikogorjan@users.noreply.github.com>
2025-02-26 16:26:51 +0100

    feat(SwapSendCard): Swap/Send Card Development (#7)
    * feat(SwapSendCard): make tabs render different components
    
    * feat(SwapSendCard): swap component design implemented
    
    * feat(PickToken): prepare PickToken dialog pop up
    
    * feat(PickToken): implement select token and pass to parent
    
    * feat(PickToken): can buy and sell same token at same time
    
    * feat(PickToken): fix token props
    
    * feat(SwapCard): implement all button statuses
    
    * feat(SwapCard): implement swapping sell and buy token on absolute button click
    
    * feat(SwapCard): on buy and sell token swap, also swap the input
    
    * feat(SwapCard): if displayed values go into overflow - allow scrolling
    
    * feat(SwapCard):format currency display
    
    * feat(PickToken): implement responsive pick tocken dialog with search
    
    * feat(SwapCard): add review info accordion
    
    * feat(SwapReview): SwapReview dialog design
    
    * feat(SendCard): implemented SendCard layout
    
    * feat(SendReview): implemented SendReview dialog
    
    * feat(utils): put swap and send card functions in to utils
    
    * feat(SendaCard): dont round "0" to fixed decimals when swapping between coin amount and fiat value
    
    * feat(SendSwapCard): minimal styling adjustments
    
    * feat(SendSwapCard): remove unused stuff
    
    * feat(OverviewPage): remove unused stuff
    
    * feat(SwapCard): add some comments
    
    * feat(SendSwapCard): remove unused stuff

--- packages/stellar/web/src/sections/overview/index.tsx
+++ packages/stellar/web/src/sections/overview/index.tsx
@@ -8,9 +8,12 @@ import { Markets } from '@/components/_overview-page-components/markets/markets'
 import { MarketTable } from '@/components/_overview-page-components/market-table/market-table';
 import { createChartData, RealtimeChartData } from 'src/utils/portfolio-value-chart-series';
 import { StatCardData } from '@/types/stat-card-data';
-import { fCurrency, fCurrencyCompact, fPercent, fShortenNumber } from '@/utils/format-number';
+import { fCurrencyCompact, fShortenNumber } from '@/utils/format-number';
 import { StatCard } from '@/components/_common/stat-card';
 import { AreaChartCard, LegendValue } from '@/components/_common/area-chart-card';
+import { SwapSendCard } from '@/components/_common/swap-send-card';
+import { Token } from '@/types/token';
+import { SwapFeeInfo } from '@/types/swap-fee-info';
 
 export default function OverviewView() {
   const theme = useTheme();
@@ -19,7 +22,6 @@ export default function OverviewView() {
   // Hardcoded chart data arrays.
   // -------------------------
 
-  const portfolioValue = 7334;
   const data24h = [
     3444, 3600, 3750, 3900, 4100, 4300, 4500, 4700, 4900, 5200, 5400, 5500, 5650, 5800, 6000, 6200,
     6400, 6600, 6800, 7000, 7200, 7300, 7320, 7334,
@@ -175,6 +177,83 @@ export default function OverviewView() {
     },
   ];
 
+  const tokensList: Token[] = [
+    {
+      id: 1,
+      url: 'https://token-icons.s3.amazonaws.com/eth.png',
+      name: 'Ethereum',
+      shortname: 'ETH',
+      owned: true,
+      countstatus: 0.02106,
+      pricestatus: 2814.25,
+      featured: true,
+      address: '0x742d35Cc6634C0532925a3b844Bc454e4438f44e',
+    },
+    {
+      id: 2,
+      url: 'https://coin-images.coingecko.com/coins/images/6319/large/usdc.png?1696506694',
+      name: 'USDC',
+      shortname: 'USDC',
+      owned: false,
+      countstatus: 0,
+      pricestatus: 0.9998,
+      featured: true,
+      address: '0x742d35Cc6634C0532925a3b844Bc454e4438f44e',
+    },
+    {
+      id: 3,
+      url: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xdAC17F958D2ee523a2206206994597C13D831ec7/logo.png',
+      name: 'Tether',
+      shortname: 'USDT',
+      owned: false,
+      countstatus: 0,
+      pricestatus: 0.9999,
+      featured: true,
+      address: '0x742d35Cc6634C0532925a3b844Bc454e4438f44e',
+    },
+
+    {
+      id: 4,
+      url: 'https://coin-images.coingecko.com/coins/images/7598/large/wrapped_bitcoin_wbtc.png?1696507857',
+      name: 'Wrapped Bitcoin',
+      shortname: 'WBTC',
+      owned: false,
+      countstatus: 0,
+      pricestatus: 95799.17,
+      featured: true,
+      address: '0x742d35Cc6634C0532925a3b844Bc454e4438f44e',
+    },
+    {
+      id: 5,
+      url: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png',
+      name: 'Wrapped Ether',
+      shortname: 'WETH',
+      owned: false,
+      countstatus: 0,
+      pricestatus: 2806.75,
+      featured: true,
+      address: '0x742d35Cc6634C0532925a3b844Bc454e4438f44e',
+    },
+    {
+      id: 6,
+      url: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png',
+      name: 'Wrapped Ether',
+      shortname: 'WETH',
+      owned: false,
+      countstatus: 0,
+      pricestatus: 2806.75,
+      featured: false,
+      address: '0x742d35Cc6634C0532925a3b844Bc454e4438f44e',
+    },
+  ];
+
+  const swapFeeInfo: SwapFeeInfo = {
+    feePercentage: 0.25,
+    networkCost: 1.0,
+    priceImpact: -0.3,
+    maxSlippage: 0.5,
+  };
+
   return (
     <DashboardContent maxWidth="xl">
       <Stack spacing={1}>
@@ -197,7 +276,8 @@ export default function OverviewView() {
           />
         </Grid2>
         <Grid2 size={{ xs: 12, md: 4 }}>
-          <AssetsAndLiabilities title="Assets & Liabilities" list={_appRelated} />
+          {/*<AssetsAndLiabilities title="Assets & Liabilities" list={_appRelated} />*/}
+          <SwapSendCard tokensList={tokensList} swapFeeInfo={swapFeeInfo} />
         </Grid2>
       </Grid2>
       {/* Second row: TradingVolume items */}
